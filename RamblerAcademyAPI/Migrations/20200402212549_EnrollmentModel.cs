bb7b7c0a93c95cca5760fe46967a22384e3893c4
Christian <christiansj10@gmail.com>
2020-04-02 16:33:05 -0500

    Enrollment Model

---
+++ RamblerAcademyAPI/Migrations/20200402212549_EnrollmentModel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RamblerAcademyAPI.Migrations
+{
+    public partial class EnrollmentModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Enrollment",
+                columns: table => new
+                {
+                    CourseReferenceNumber = table.Column<int>(nullable: false),
+                    StudentId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrollments", x => new { x.CourseReferenceNumber, x.StudentId });
+                    table.ForeignKey(
+                        name: "FK_Enrollments_CourseSection_CourseReferenceNumber",
+                        column: x => x.CourseReferenceNumber,
+                        principalTable: "CourseSection",
+                        principalColumn: "CourseReferenceNumber",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Enrollments_Users_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollments_StudentId",
+                table: "Enrollment",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enrollment");
+        }
+    }
+}
---
+++ RamblerAcademyAPI/Models/Enrollment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RamblerAcademyAPI.Models
+{
+    public class Enrollment
+    {
+        [ForeignKey("CourseSection")]
+        public int CourseReferenceNumber { get; set; }
+        public CourseSection CourseSection { get; set; }
+
+        [ForeignKey("User")]
+        public int StudentId { get; set; }
+        public User Student { get; set; }
+    }
+}
