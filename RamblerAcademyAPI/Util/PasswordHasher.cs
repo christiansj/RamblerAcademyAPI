ce8e96f7e1b9fe3d7bc3346be41c2f111259d61a
Christian <christiansj10@gmail.com>
2020-04-03 00:49:23 -0500

    HashPassword Utility

---
+++ RamblerAcademyAPI/Util/PasswordHasher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace RamblerAcademyAPI.Util
+{
+    public class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256/ 8
+            ));
+
+            return hashed;
+        }
+
+    }
+}
