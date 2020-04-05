93e465e3861ec935d60340828495f7c505cc2422
Christian <christiansj10@gmail.com>
2020-04-05 16:15:18 -0500

    Subject Queries

---
+++ RamblerAcademyAPI/GraphQL/GraphQLQueries/SubjectQuery.cs
@@ -0,0 +1,27 @@
+﻿using GraphQL.Types;
+using RamblerAcademyAPI.Contracts;
+using RamblerAcademyAPI.GraphQL.GraphQLTypes;
+
+
+namespace RamblerAcademyAPI.GraphQL.GraphQLQueries
+{
+    public class SubjectQuery: ObjectGraphType, IGraphQLQuery
+    {
+        public SubjectQuery(ISubjectRepository repository)
+        {
+            Field<ListGraphType<SubjectType>>(
+                "subjects",
+               resolve: context => repository.GetAll()
+            );
+
+            Field<SubjectType>(
+                "subject",
+                arguments: new QueryArguments(new
+                    QueryArgument<IdGraphType>
+                { Name = "id" }),
+                resolve:
+                    context => repository.GetSubjectById(context.GetArgument<int>("id"))
+            );
+        }
+    }
+}
