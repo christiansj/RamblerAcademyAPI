3fa1876d7de9a7ee2c19f3d092264a5f702f0a02
Christian <christiansj10@gmail.com>
2020-04-10 15:59:51 -0500

    Day API endpoints

---
+++ RamblerAcademyAPI/Controllers/DayController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RamblerAcademyAPI.GraphQL.GraphQLConsumers;
+using RamblerAcademyAPI.GraphQL.GraphQLUserErrors;
+using RamblerAcademyAPI.Models;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RamblerAcademyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class DayController : Controller
+    {
+        private readonly DayConsumer _consumer;
+        public DayController(DayConsumer consumer)
+        {
+            _consumer = consumer;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            IEnumerable<Day> days = await _consumer.GetAllDaysAsync();
+            return Ok(days);
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            Day day = await _consumer.GetDayByIdAsync(id);
+            if(day == null)
+            {
+                return NotFound();
+            }
+            return Ok(day);
+        }
+    }
+}
---
+++ RamblerAcademyAPI/GraphQL/GraphQLConsumers/DayConsumer.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using RamblerAcademyAPI.GraphQL.Client;
+using RamblerAcademyAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using RamblerAcademyAPI.GraphQL.GraphQLConsumers.Util;
+
+namespace RamblerAcademyAPI.GraphQL.GraphQLConsumers
+{
+    public class DayConsumer
+    {
+        private readonly GraphQLClient _client;
+        private string dayFragment = @"
+            id
+            name
+        ";
+        public DayConsumer(GraphQLClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IEnumerable<Day>> GetAllDaysAsync()
+        {
+            string query = string.Format(@"
+                {{
+                    days{{
+                        {0}
+                    }}
+                }}
+            ", dayFragment);
+
+            string resultString = await _client.Query(query);
+            var data = DataParser.ParseDataFromString(resultString, "days");
+            return JsonConvert.DeserializeObject<IEnumerable<Day>>(data);
+        }
+
+        public async Task<Day> GetDayByIdAsync(int dayId)
+        {
+            string query = string.Format(@"
+                {{
+                    day(id: {0}){{
+                        {1}
+                    }}
+                }}
+            ", dayId, dayFragment);
+
+            string resultString = await _client.Query(query);
+            var data = DataParser.ParseDataFromString(resultString, "day");
+            return JsonConvert.DeserializeObject<Day>(data);
+        }
+    }
+}
